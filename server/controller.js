a9b2850fa384e3033d6cb03b989188255bb386b0
Lorin Knudson <lcknudson@gmail.com>
2023-04-28 19:57:24 -0600

    fortune feature

---
+++ server/controller.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["Not every cage is a prison, not every loss eternal", "Doctor, if I were able to show emotion, your new infatuation with that term 'logic' would begin to annoy me.", "Man is born to live and not prepared to live.", "Practice permanent, perfect practice makes perfect permanent."];
+
+        //choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
